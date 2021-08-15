dd197b1285001b986a95cf4df8de087c8e9b6a19
onefiter <onefiter@gmail.com>
2021-08-15 13:50:30 +0800

    props使用

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-import React, { Component } from 'react'
+import React, {
+    Component
+} from 'react'
 import Table from './Table'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <Table />
-      </div>
-    )
-  }
+    state = {
+        characters : [
+                 {
+                     name: 'Charlie',
+                     job: 'Janitor',
+                 },
+                 {
+                     name: 'Mac',
+                     job: 'Bouncer',
+                 },
+                 {
+                     name: 'Dee',
+                     job: 'Aspring actress',
+                 },
+                 {
+                     name: 'Dennis',
+                     job: 'Bartender',
+                 },
+             ]
+       }
+
+    render() {
+        const characters = [{
+                name: 'Charlie',
+                job: 'Janitor',
+            },
+            {
+                name: 'Mac',
+                job: 'Bouncer',
+            },
+            {
+                name: 'Dee',
+                job: 'Aspring actress',
+            },
+            {
+                name: 'Dennis',
+                job: 'Bartender',
+            },
+        ]
+
+        return ( <div className = "container" >
+            <Table  characterData={characters}  />
+            </div>
+        )
+    }
 }
 
 export default App
